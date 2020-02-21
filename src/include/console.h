568470c6d1941738df2c0d093432100bbb7591b3
BugenZhao <bugenzhao@qq.com>
2020-02-21 14:42:21 +0800

    Switch to CMake

--- src/include/console.h
+++ src/include/console.h
@@ -9,4 +9,4 @@
 
 void print_pos(void *vram, int8_t row, int8_t col, const char *str, color_t color);
 
-#endif //LITCHI_CONSOLE_H
+#endif // LITCHI_CONSOLE_H
--- src/kernel/entry.c
+++ src/kernel/entry.c
@@ -6,10 +6,9 @@
 #include "types.h"
 
 int kernel_entry() {
-    uint8_t *   vram  = (uint8_t *) 0xB8000;
-    const char *msg =
-        "Hello, Litchi!\n\nThis is the loader of Bugen's tiny OS -- Litchi OS.\n"
-        "Version v0.2.0, Build 200220\n\n(C) BugenZhao 2020";
+    uint8_t *   vram = (uint8_t *) 0xB8000;
+    const char *msg  = "Hello, Litchi!!\n\nThis is the loader of Bugen's tiny OS -- Litchi OS.\n"
+                      "Version v0.2.0, Build 200220\n\n(C) BugenZhao 2020";
     print_pos(vram, 4, 0, msg, BYELLOW);
 
     return 0;
