7c44fe64244a7dcb6382d1df9f111f573a652b16
BugenZhao <i@bugenzhao.com>
2020-03-26 12:25:47 +0800

    Port some headers from old Litchi

---
+++ README.md
@@ -0,0 +1,11 @@
+# Litchi
+Bugen's toy kernel.
+
+## Progress
+- [x] Adopt bootloader from JOS / xv6
+- [ ] Port old Litchi codes
+- [ ] Kernel level standard output
+- [ ] Color output in console
+- [ ] Kernel level standard input
+- [ ] Basic kernel commmands
+- [ ] ...
---
+++ include/color.h
@@ -0,0 +1,27 @@
+//
+// Created by Bugen Zhao on 2020/3/26.
+//
+
+#ifndef LITCHI_COLOR_H
+#define LITCHI_COLOR_H
+
+enum color_t {
+    BLACK = 0b0000,
+    BLUE,
+    GREEN,
+    CYAN,
+    RED,
+    MAGENTA,
+    BROWN,
+    GRAY,
+    DARK_GRAY,
+    LIGHT_BLUE,
+    LIGHT_GREEN,
+    LIGHT_CYAN,
+    LIGHT_RED,
+    LIGHT_MAGENTA,
+    YELLOW,
+    WHITE
+};
+
+#endif //LITCHI_COLOR_H
